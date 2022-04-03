de1db6ef476ee78f41000ce58eded43462deca64
Roma <romachne@gmail.com>
2022-04-03 16:56:53 +0300

    Initial commit

---
+++ src/zadanie1.java
@@ -0,0 +1,10 @@
+public class zadanie1 {
+    public static void main(String[] args) {
+//        int[] intArray;
+//        intArray = new int[100];
+        int intArray[] = new int[100];
+        int arrayLength = intArray.length;
+
+        System.out.println(arrayLength);
+    }
+}
