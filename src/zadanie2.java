22e766e1eb3fb7ea8eb3024b9b42b9769176cd69
Roma <romachne@gmail.com>
2022-04-04 02:34:00 +0300

    gotovo

--- src/zadanie2.java
+++ src/zadanie2.java
@@ -1,112 +1,153 @@
-class disorderArray2 {
+import java.util.Arrays;
+
+interface arr {
+    int size = 15;
+}
+
+class disorderArray2 implements arr {
     private long[] a;
-    private int nElems;
 
-    public disorderArray2(int size) {
+    public disorderArray2() {
         a = new long[size];
+        for(int i=0; i<size; i++)
+            a[i] = (int) (Math.random()*size);
     }
-
-    public boolean find(long searchKey) {
-        int j;
-
-        for(j=0; j<nElems; j++)
-            if(a[j] == searchKey)
-                break;
-
-        return j != nElems;
-    }
-    public void insert(long value) {
-        a[nElems] = value;
-        nElems++;
-    }
-    public boolean delete(long value) {
-        int j;
-
-        for(j=0; j<nElems; j++)
-            if(value == a[j])
-                break;
-        if(j==nElems)
-            return false;
-        else {
-            for(int k=j; k<nElems; k++)
-                a[k] = a[k+1];
-
-            nElems--;
-            return true;
-        }
-    }
-    public void vivod() {
-        for(int k=0; k<a.length; k++)
-            System.out.println(a[k]);
-    }
-    public void bubbleSort() {
-        int out, in;
-        for (out=nElems-1; out>1; out--)
-            for(in=0; in<out; in++)
-                if(a[in]>a[in+1])
-                    swap(in, in+1);
-    }
-    private void swap(int one, int two) {
-        long temp = a[one];
-        a[one] = a[two];
-        a[two] = temp;
+    public long[] get() {
+        return a;
     }
     public void choiceSort() {
-        System.out.println(System.currentTimeMillis());
-        long max = 0, c;
-        int Nmax, i, k;
+        long min, c;
+        int Nmin, i, k;
 
-        for(i=0; i<nElems; i++) {
-            max = a[i];
-            Nmax = i;
+        for(i=0; i<a.length; i++) {
+            min = a[i];
+            Nmin = i;
 
-            for(k=0; k<nElems; k++)
-                if(a[k]>max) {
-                    max=a[k];
-                    Nmax=k;
+            for(k=i+1; k<a.length; k++)
+                if(a[k]<min) {
+                    min=a[k];
+                    Nmin=k;
                 }
-            c=a[i];
-            a[i]=max;
-            a[Nmax]=c;
-        }
 
-        System.out.println(System.currentTimeMillis());
+            c=a[i];
+            a[i]=min;
+            a[Nmin]=c;
+        }
     }
     public void insertSort() {
-        System.out.println(System.currentTimeMillis());
         int i, j;
         long temp;
 
-        for(i=1;i<nElems-1;i++) {
+        for(i=1;i<size;i++) {
             temp=a[i];
-            j=i;
-
-            if((a[j-1]>temp)&(j>0)) {
-                a[j]=a[j-1];
-                j=j-1;
+            j=i-1;
+            for(; j>=0; j--) {
+                if(a[j]>temp) {
+                    a[j+1]=a[j];
+                } else {
+                    break;
+                }
             }
 
-            a[j]=temp;
+            a[j+1]=temp;
         }
+    }
+    public void fastSort() {
+        quickSort(0, size-1);
+    }
+    private void quickSort(int low, int high) {
+        int middle = low+(high-low)/2, i = low, j = high;
+        long opora = a[middle], temp;
 
-        System.out.println(System.currentTimeMillis());
+        while(i<=j) {
+            while(a[i] < opora) {
+                i++;
+            }
+            while(a[j] > opora) {
+                j--;
+            }
+            if(i<=j) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if(j>low)
+            quickSort(low, j);
+        if(high>i)
+            quickSort(i, high);
+    }
+    public long[] mergeSort() {
+        return mergeSortSort(a);
+    }
+    private long[] mergeSortSort(long[] a) {
+        if(a == null) {
+            return null;
+        }
+        if(a.length<2) {
+            return a;
+        }
+        long[] a2 = new long[a.length/2];
+        System.arraycopy(a, 0, a2, 0, a.length/2);
+        long[] a3 = new long[a.length - a.length/2];
+        System.arraycopy(a, a.length/2, a3, 0, a.length - a.length/2);
+        a2 = mergeSortSort(a2);
+        a3 = mergeSortSort(a3);
+
+        return mergeMergeSort(a2, a3);
+    }
+    private long[] mergeMergeSort(long[] a, long[] a2) {
+        long[] a3 = new long[a.length + a2.length];
+        int posA = 0, posB = 0;
+
+        for(int i=0;i<a3.length;i++) {
+            if (posA > a.length-1) {
+                long aa = a2[posB];
+                a3[i] = aa;
+                posB++;
+            }
+            else if (posB > a2.length-1) {
+                long aa = a[posA];
+                a3[i] = aa;
+                posA++;
+            }
+            else if (a[posA] < a2[posB]) {
+                long aa = a[posA];
+                a3[i] = aa;
+                posA++;
+            }
+            else {
+                long b = a2[posB];
+                a3[i] = b;
+                posB++;
+            }
+        }
+        return a3;
     }
 }
 
 public class zadanie2 {
     public static void main(String[] args) {
-        disorderArray2 newArray = new disorderArray2(10);
-        newArray.insert(10);
-        newArray.insert(1);
-        newArray.insert(3);
-        newArray.insert(7);
-        newArray.insert(14);
-        newArray.vivod();
-        System.out.println("Sorted");
-//        newArray.bubbleSort();
-        newArray.choiceSort();
-        System.out.println("Sorted");
-        newArray.insertSort();
-        newArray.vivod();
+        disorderArray2 arrayForIns = new disorderArray2();
+        disorderArray2 arrayForCho = arrayForIns;
+        disorderArray2 arrayForMer = arrayForIns;
+        disorderArray2 arrayForFas = arrayForIns;
+
+        System.out.println("Вставка");
+        arrayForIns.insertSort();
+        System.out.println(Arrays.toString(arrayForIns.get()));
+
+        System.out.println("Выбор");
+        arrayForCho.choiceSort();
+        System.out.println(Arrays.toString(arrayForCho.get()));
+
+        System.out.println("Слияние");
+        System.out.println(Arrays.toString(arrayForMer.mergeSort()));
+
+        System.out.println("Быстрая");
+        arrayForFas.fastSort();
+        System.out.println(Arrays.toString(arrayForFas.get()));
     }
 }
