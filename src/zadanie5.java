ecd5dd515cf7bbedf02249662f6dc0e6b40b294b
Roma <romachne@gmail.com>
2022-09-19 06:40:40 +0300

    tree delete

--- src/zadanie5.java
+++ src/zadanie5.java
@@ -64,9 +64,72 @@ class Tree
         return  current;
     }
 
-    public void delete(int id)
-    {
+    public TreeNode delete(int key) {
+        TreeNode current = root;
+        TreeNode parent = root;
+        boolean isLeftChild = true;
 
+        while (current.data != key) {
+            parent = current;
+
+            if (key < current.data) {
+                isLeftChild = true;
+                current = current.leftChild;
+            } else {
+                isLeftChild = false;
+                current = current.rightChild;
+            }
+
+            if (current == null)
+                return null;
+        }
+
+        if (current.leftChild == null && current.rightChild == null)
+            if (current == root)
+                root = null;
+            else if (isLeftChild)
+                parent.leftChild = null;
+            else
+                parent.rightChild = null;
+        else if (current.rightChild == null)
+            if (current == root)
+                root = current.leftChild;
+            else if (isLeftChild)
+                parent.leftChild = current.leftChild;
+            else
+                parent.rightChild = current.rightChild;
+        else if (current.leftChild == null)
+            if (current == root)
+                root = current.rightChild;
+            else if (isLeftChild)
+                parent.leftChild = current.rightChild;
+            else
+                parent.rightChild = current.rightChild;
+        else {
+            TreeNode successor = current.rightChild;
+            if (successor.leftChild == null) {
+                if (isLeftChild) {
+                    parent.leftChild = successor;
+                    successor.leftChild = current.leftChild;
+                } else {
+                    parent.rightChild = successor;
+                    successor.leftChild = current.leftChild;
+                }
+            } else {
+                TreeNode successorParent = successor;
+
+                while (successor.leftChild != null) {
+                    successorParent = successor;
+                    successor = successor.leftChild;
+                }
+
+                successorParent.leftChild = successor.rightChild;
+                successor.rightChild = current.rightChild;
+                parent.rightChild = successor;
+                successor.leftChild = current.leftChild;
+            }
+        }
+        return current;
     }
 }
 
